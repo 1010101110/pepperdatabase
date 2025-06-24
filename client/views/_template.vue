8469832cd178024217cdf30567a5e93f5009f7a7
1010101110 <sirs1yaer@gmail.com>
2025-06-24 00:22:55 -0700

    template for pages

---
+++ client/views/_template.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="text-center">
+
+  </v-container>
+</template>
+
+<script setup>
+// init
+import { ref, inject } from 'vue';
+
+// reactive state
+const snacks = inject('snacks')
+
+
+</script>
