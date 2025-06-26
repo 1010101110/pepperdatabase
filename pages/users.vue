c16612b6d94cca93303ddcb13a0e2cef22c70db6
1010101110 <sirs1yaer@gmail.com>
2025-06-25 19:53:01 -0700

    user list

---
+++ pages/users.vue
@@ -0,0 +1,41 @@
+<script setup>
+// reactive state
+const snacks = inject('snacks')
+
+const {data} = await useFetch('/api/user/list')
+
+useHead({
+  title: 'PDB Users'
+})
+</script>
+
+<template>
+  <v-container>
+        <v-row>
+          <v-col cols="12" v-for="u in data" :key="u.id">
+            <v-card class="pa-2 mb-2">
+              <div class="d-inline-flex mx-2">
+                <NuxtLink :to="`/user/${u.id}`">
+                  <v-img
+                    :width="100"
+                    cover
+                    :src="`https://pepperdatabase.org/images/avatars/${u.id}.jpg`"
+                  ></v-img>
+                </NuxtLink>
+              </div>
+              <div class="d-inline-flex mx-2">
+                <span>{{ u.name }}</span>
+              </div>
+              <div class="d-inline-flex mx-2">
+                <v-btn :href="`/user/${u.id}`">details</v-btn>
+              </div>
+              <div class="d-clear"></div>
+              <div class="d-inline-flex mx-2">
+                <span>{{ u.bio }}</span>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+
+  </v-container>
+</template>
