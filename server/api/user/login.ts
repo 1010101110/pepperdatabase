aee9bc26fc8e89c242634106f294ec96fec78dce
1010101110 <sirs1yaer@gmail.com>
2025-06-25 19:52:30 -0700

    fix auth cookie

--- server/api/user/login.ts
+++ server/api/user/login.ts
@@ -3,14 +3,14 @@ import { setCookie } from 'h3'
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   try{
-    const [resp] = await db.execute(`select name, email, bio, avatar from users2 where activation_hash=${body.activation_hash} and deleted = 0`)
+    const [resp] = await db.execute(`select id, name, email, bio, avatar from users2 where activation_hash=${body.activation_hash} and deleted = 0`)
     const user = (resp as any[])[0]
     if (!user) {
       throw createError({ statusCode: 401, statusMessage: 'Invalid credentials' })
     }
 
     //store in cookie for autologin
-    setCookie(event, 'auth_token', user.activation_hash, {
+    setCookie(event, 'auth_token', body.activation_hash, {
       httpOnly: true,
       path: '/',
       sameSite: 'lax',
