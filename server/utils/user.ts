fd07212002c052561c85bd158a02930f50d93657
1010101110 <sirs1yaer@gmail.com>
2025-06-30 10:56:03 -0700

    comments

--- server/utils/user.ts
+++ server/utils/user.ts
@@ -2,16 +2,14 @@ import { getCookie, H3Event, createError } from 'h3'
 import db from '~/server/utils/db'
 
 export async function getUserFromRequest(event: H3Event) {
+  //token from cookie
   const token = getCookie(event, 'auth_token')
-
   if (!token) {
     throw createError({ statusCode: 401, statusMessage: 'Unauthorized' })
   }
 
-  // In real apps, you’d verify a JWT or session here.
-  // For now, assume the token is a user ID (string)
+  //check db
   const [results] = await db.execute('SELECT * FROM users2 WHERE activation_hash = ?', [token]) as any
-
   const user = results?.[0]
   if (!user) {
     throw createError({ statusCode: 401, statusMessage: 'Invalid user' })
