c2ad5cab707dbaa563add7b7ea48ad44d810e5c3
1010101110 <sirs1yaer@gmail.com>
2025-06-30 10:35:47 -0700

    correct implentation of icons

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,8 +3,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-import colors from 'vuetify/util/colors'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
@@ -29,6 +28,13 @@ export default defineNuxtPlugin((nuxtApp) => {
           }
         }
       }
+    },
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
     },
   })
 
