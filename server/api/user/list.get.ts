3589c0a65c0cce05c873c0aa902e054d633fbbf8
1010101110 <sirs1yaer@gmail.com>
2025-07-02 14:22:54 -0700

    don't show deleted users

--- server/api/user/list.get.ts
+++ server/api/user/list.get.ts
@@ -2,7 +2,7 @@ import db from "~/server/utils/db";
 
 export default defineEventHandler(async (event) => {
   const [results] = await db.execute(
-    "SELECT id,name,bio,avatar FROM users2 where active = true",
+    "SELECT id,name,bio,avatar FROM users2 where active = true and deleted = false",
   );
   return results;
 });
